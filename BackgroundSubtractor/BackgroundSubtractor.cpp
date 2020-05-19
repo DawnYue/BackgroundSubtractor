e57845fb0249316eeacf712818328ea041cdf472
Dawn Yue <1179348863@qq.com>
2020-05-19 21:20:03 +0800

    课前准备

--- BackgroundSubtractor/BackgroundSubtractor.cpp
+++ BackgroundSubtractor/BackgroundSubtractor.cpp
@@ -1,20 +1,30 @@
-﻿// BackgroundSubtractor.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>  
+#include <opencv2\core\core.hpp>  
+#include <opencv2\highgui\highgui.hpp>  
+#include <opencv2\imgproc\imgproc.hpp>  
+#include<cmath>
+#include<opencv2/opencv.hpp>
+#include<iostream>
+#include<math.h>
+#include <vector>
+#include "cv.h"
+using namespace cv;
+using namespace std;
 
-#include <iostream>
+//课前准备
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+int main() {
+	VideoCapture video("1.avi");
+	Mat frame, mask, thresholdImage, output;
+	video >> frame;
+	BackgroundSubtractorMOG bgSubtractor(20, 10, 0.5, false);
+	while (true) {
+		video >> frame;
+		++frameNum;
+		bgSubtractor(frame, mask, 0.001);
+		imshow("mask", mask);
+		waitKey(10);
+	}
+	return 0;
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+、
