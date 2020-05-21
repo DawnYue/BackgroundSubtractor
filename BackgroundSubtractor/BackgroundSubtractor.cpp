553e519c6da7d582e90123f5cd5615053ca0218f
Dawn Yue <1179348863@qq.com>
2020-05-21 15:55:34 +0800

    课前准备（老师）

--- BackgroundSubtractor/BackgroundSubtractor.cpp
+++ BackgroundSubtractor/BackgroundSubtractor.cpp
@@ -8,23 +8,38 @@
 #include<math.h>
 #include <vector>
 #include "cv.h"
-using namespace cv;
+
+//课前准备（老师）
+//https://github.com/lizhu1126/dip-class-demos/blob/feature12.bgSub/dip-class-demos/funtions.cpp
 using namespace std;
+using namespace cv;
 
-//课前准备
+//打开摄像头，手动选取视频中的一个像素，并描画在时间方向上的灰度分布
+int verifyGaussian()
+{
 
-int main() {
-	VideoCapture video("1.avi");
-	Mat frame, mask, thresholdImage, output;
-	video >> frame;
-	BackgroundSubtractorMOG bgSubtractor(20, 10, 0.5, false);
-	while (true) {
-		video >> frame;
-		++frameNum;
-		bgSubtractor(frame, mask, 0.001);
-		imshow("mask", mask);
-		waitKey(10);
-	}
+
+
+
+
 	return 0;
+}
 
-、
+
+int main()
+{
+	//开始计时
+	double start = static_cast<double>(cvGetTickCount());
+
+	//该demo验证并演示，视频中的像素灰度值变换是否呈高斯分布
+	verifyGaussian();
+
+	//结束计时
+	double time = ((double)cvGetTickCount() - start) / cvGetTickFrequency();
+	//显示时间
+	cout << "processing time:" << time / 1000 << "ms" << endl;
+
+	//等待键盘响应，按任意键结束程序
+	system("pause");
+	return 0;
+}
