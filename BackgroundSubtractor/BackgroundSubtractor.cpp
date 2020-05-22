e8b44480364294f6010db07efa2695ff06470efb
Dawn Yue <1179348863@qq.com>
2020-05-22 15:00:23 +0800

    练习2

--- BackgroundSubtractor/BackgroundSubtractor.cpp
+++ BackgroundSubtractor/BackgroundSubtractor.cpp
@@ -15,10 +15,8 @@ using namespace cv;
 
 int calcGaussianBackground(std::vector<cv::Mat> srcMats, cv::Mat & meanMat, cv::Mat &varMat)
 {
-
 	int rows = srcMats[0].rows;
 	int cols = srcMats[0].cols;
-
 	for (int h = 0; h < rows; h++)
 	{
 		for (int w = 0; w < cols; w++)
@@ -26,13 +24,13 @@ int calcGaussianBackground(std::vector<cv::Mat> srcMats, cv::Mat & meanMat, cv::
 			int sum = 0;
 			float var = 0;
 			for (int i = 0; i < srcMats.size(); i++) {
-				sum += srcMats[i].at<uchar>(h, w);//均值
+				sum += srcMats[i].at<uchar>(h, w);
 			}
-			meanMat.at<uchar>(h, w) = sum / srcMats.size();
-			for (int i = 0; i < srcMats.size(); i++) {//方差
+			meanMat.at<uchar>(h, w) = sum / srcMats.size();//均值
+			for (int i = 0; i < srcMats.size(); i++) {
 				var += pow((srcMats[i].at<uchar>(h, w) - meanMat.at<uchar>(h, w)), 2);
 			}
-			varMat.at<float>(h, w) = var / srcMats.size();
+			varMat.at<float>(h, w) = var / srcMats.size();//方差
 		}
 	}
 	return 0;
@@ -54,9 +52,7 @@ int gaussianThreshold(cv::Mat srcMat, cv::Mat meanMat, cv::Mat varMat, float wei
 			meanI = meanMat.at<uchar>(h, w);
 			int dif = abs(srcI - meanI);
 			int th = weight * varMat.at<float>(h, w);
-
 			if (dif > th) {
-
 				dstMat.at<uchar>(h, w) = 255;
 			}
 			else {
@@ -70,14 +66,13 @@ int gaussianThreshold(cv::Mat srcMat, cv::Mat meanMat, cv::Mat varMat, float wei
 int main()
 {
 	VideoCapture capVideo(0);
-	//打开失败
-	if (!capVideo.isOpened()) {
+	if (!capVideo.isOpened()) {//打开失败
 		std::cout << "can not open" << std::endl;
 		return -1;
 	}
 
-	std::vector<cv::Mat> srcMats;	//计算背景模型的图像
-	int nBg = 200;					//建立背景模型的数量
+	std::vector<cv::Mat> srcMats;	//背景模型图像
+	int nBg = 200;					//背景模型数量
 	float wVar = 1;					//方差权重
 	int cnt = 0;
 	cv::Mat frame;
@@ -89,8 +84,7 @@ int main()
 	{
 		capVideo >> frame;
 		cvtColor(frame, frame, COLOR_BGR2GRAY);
-	
-		if (cnt < nBg) {//前面的nBg帧，计算背景
+		if (cnt < nBg) {//计算背景
 			srcMats.push_back(frame);
 			if (cnt == 0) {
 				std::cout << "reading frame " << std::endl;
