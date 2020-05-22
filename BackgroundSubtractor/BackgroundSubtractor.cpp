13ef824a5fc076fc1600bd2131fd47cc18212294
Dawn Yue <1179348863@qq.com>
2020-05-22 14:40:42 +0800

    练习2

--- BackgroundSubtractor/BackgroundSubtractor.cpp
+++ BackgroundSubtractor/BackgroundSubtractor.cpp
@@ -9,16 +9,74 @@
 #include <vector>
 #include "cv.h"
 
-//练习1
+//练习2
 using namespace std;
 using namespace cv;
 
-int bgSub_demo()
+int calcGaussianBackground(std::vector<cv::Mat> srcMats, cv::Mat & meanMat, cv::Mat &varMat)
 {
 
+	int rows = srcMats[0].rows;
+	int cols = srcMats[0].cols;
+
+
+	for (int h = 0; h < rows; h++)
+	{
+		for (int w = 0; w < cols; w++)
+		{
+
+			int sum = 0;
+			float var = 0;
+			//求均值
+			for (int i = 0; i < srcMats.size(); i++) {
+				sum += srcMats[i].at<uchar>(h, w);
+			}
+			meanMat.at<uchar>(h, w) = sum / srcMats.size();
+			//求方差
+			for (int i = 0; i < srcMats.size(); i++) {
+				var += pow((srcMats[i].at<uchar>(h, w) - meanMat.at<uchar>(h, w)), 2);
+			}
+			varMat.at<float>(h, w) = var / srcMats.size();
+		}
+	}
+
+	return 0;
+}
+int gaussianThreshold(cv::Mat srcMat, cv::Mat meanMat, cv::Mat varMat, float weight, cv::Mat & dstMat)
+{
+	int srcI;
+	int meanI;
+	int dstI;
+	int rows = srcMat.rows;
+	int cols = srcMat.cols;
+
+	for (int h = 0; h < rows; h++)
+	{
+		for (int w = 0; w < cols; w++)
+		{
+			srcI = srcMat.at<uchar>(h, w);
+			meanI = meanMat.at<uchar>(h, w);
+			int dif = abs(srcI - meanI);
+			int th = weight * varMat.at<float>(h, w);
+
+			if (dif > th) {
+
+				dstMat.at<uchar>(h, w) = 255;
+			}
+			else {
+				dstMat.at<uchar>(h, w) = 0;
+			}
+		}
+	}
+
+	return 0;
+}
+
+int bgSubGaussian_demo()
+{
 	//----------------------读取视频文件--------------------------
-	//VideoCapture capVideo("E:\\1\\2.MP4");
 	VideoCapture capVideo(0);
+	//VideoCapture capVideo("E:\\1\\2.MP4");
 
 	//如果视频打开失败
 	if (!capVideo.isOpened()) {
@@ -26,44 +84,64 @@ int bgSub_demo()
 		return -1;
 	}
 
-	//计数器
-	int cnt = 0;
-	Mat frame;
-	Mat bgMat;
-	Mat subMat;
-	Mat bny_subMat;
-	Mat bgModelMat;
+	//用来计算背景模型的图像
+	std::vector<cv::Mat> srcMats;
 
-	while (1) {
+	//参数设置
+	int nBg = 200;		//用来建立背景模型的数量
+	float wVar = 1;		//方差权重
+
+	int cnt = 0;
+	cv::Mat frame;
+	cv::Mat meanMat;
+	cv::Mat varMat;
+	cv::Mat dstMat;
+
+	while (true)
+	{
 		capVideo >> frame;
 		cvtColor(frame, frame, COLOR_BGR2GRAY);
-		if (cnt == 0) {
-			//第一帧，获得背景图像
-			frame.copyTo(bgMat);
+
+		//前面的nBg帧，计算背景
+		if (cnt < nBg) {
+
+			srcMats.push_back(frame);
+
+			if (cnt == 0) {
+				std::cout << "reading frame " << std::endl;
+			}
+
+		}
+		else if (cnt == nBg) {
+			//计算模型
+			meanMat.create(frame.size(), CV_8UC1);
+			varMat.create(frame.size(), CV_32FC1);
+			std::cout << "calculating background models" << std::endl;
+			calcGaussianBackground(srcMats, meanMat, varMat);
 		}
 		else {
-			//第二帧开始背景差分
-			//背景图像和当前图像相减
-			absdiff(frame, bgMat, subMat);
-			//差分结果二值化
-			threshold(subMat, bny_subMat, 20, 255, CV_THRESH_BINARY);
-
-			imshow("b_subMat", bny_subMat);
+			//背景差分
+			dstMat.create(frame.size(), CV_8UC1);
+			gaussianThreshold(frame, meanMat, varMat, wVar, dstMat);
+			imshow("result", dstMat);
 			imshow("frame", frame);
 			waitKey(30);
 		}
+
 		cnt++;
 
 	}
+
 	return 0;
 }
+
 
 int main()
 {
 	//开始计时
 	double start = static_cast<double>(cvGetTickCount());
 
-	bgSub_demo();
+	bgSubGaussian_demo();
 
 	//结束计时
 	double time = ((double)cvGetTickCount() - start) / cvGetTickFrequency();
